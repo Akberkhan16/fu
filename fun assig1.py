b4d83c082beeec3e2ea86eaca5eeb19607a9a0eb
Akberkhan16 <94068972+Akberkhan16@users.noreply.github.com>
2021-11-21 17:45:47 +0530

    Add files via upload

---
+++ fun assig1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[18]:
+
+
+numbers =(8,2,3,0,7)
+def add(numbers):
+    count=0
+    for i in range(len(numbers)):
+        count+=numbers[i]
+    return count
+
+    
+
+
+# In[19]:
+
+
+add(numbers)
+
+
+# In[ ]:
+
+
+
+
